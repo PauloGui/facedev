7406e944dce706b2ef2608c122ff7eca9d2af490
Paulo Sousa <paulopgss@gmail.com>
2020-12-14 11:56:01 -0200

    Configurado o styles global

--- src/pages/LandingPage/index.jsx
+++ src/pages/LandingPage/index.jsx
@@ -3,7 +3,9 @@ import React from 'react'
 import { Container } from './styles'
 
 function LandingPage() {
-  return <div />
+  return(
+      <Container></Container>
+  )
 }
 
 export default LandingPage
